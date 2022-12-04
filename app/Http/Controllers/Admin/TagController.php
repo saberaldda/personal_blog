065d3607640e78e1664caae5c62e345d1ff9ec17
The Dark Saber <saberaldda@gmail.com>
2022-12-04 09:26:59 +0200

    blog

---
+++ app/Http/Controllers/Admin/TagController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Tag;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\EditTagRequest;
+use App\Http\Requests\Admin\CreateTagRequest;
+
+class TagController extends Controller
+{
+    public function index()
+    {
+        $tags = Tag::paginate(20);
+
+        return view('admin.tags.index', compact('tags'));
+    }
+
+    public function create()
+    {
+        return view('admin.tags.create');
+    }
+
+    public function store(CreateTagRequest $request)
+    {
+        Tag::create($request->validated());
+
+        return redirect()->route('admin.tags.index');
+    }
+
+    public function edit(Tag $tag)
+    {
+        return view('admin.tags.edit', compact('tag'));
+    }
+
+    public function update(EditTagRequest $request, Tag $tag)
+    {
+        $tag->update($request->validated());
+
+        return redirect()->route('admin.tags.index');
+    }
+
+    public function destroy(Tag $tag)
+    {
+        foreach ($tag->posts as $post) {
+            $post->tags()->detach($tag);
+        }
+
+        if (!$tag->posts()->count()) {
+            $tag->delete();
+        }
+
+        return redirect()->route('admin.tags.index');
+    }
+}
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Latest posts</div>
+
+                    <div class="card-body">
+                        <div class="col">
+                            @foreach ($posts as $post)
+                                <div class="col-md-4">
+                                    <div>
+                                        <a href="{{ route('posts.show', $post) }}">
+                                            @if($post->image)
+                                                <img src="{{ asset('storage/uploads/' . $post->image) }}" width="100"
+                                                     height="100" class="img-fluid">
+                                            @else
+                                                <img src="{{ asset('storage/' . 'no_image.jpg') }}" width="100" height="100"
+                                                     class="img-fluid">
+                                            @endif
+                                        </a>
+                                    </div>
+                                    <div class="d-flex justify-content-between">
+                                        <div class="text-muted">{{ $post->created_at->format('d F Y') }}</div>
+                                        <div class=""><a href="#"
+                                                         class="font-weight-bolder text-info">{{ $post->category->name }}</a>
+                                            <div class="flex flex-wrap">
+                                            @foreach ($post->tags as $tag)
+                                                <span
+                                                    class="mr-3 text-sm font-medium uppercase">#{{ $tag->name }}</span>
+                                            @endforeach
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a href="{{ route('posts.show', $post) }}" class="font-weight-bold text-success">
+                                        <h3 class=""">{{ $post->title }}</h3>
+                                    </a>
+                                    <p class="text-muted">{{ $post->post }}</p>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
